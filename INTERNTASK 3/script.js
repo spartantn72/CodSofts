bac454536897e25b01c4cc91c827b8704fdf2b2c
Sankaranarayanan S <165315431+spartantn72@users.noreply.github.com>
2024-06-18 15:35:03 +0530

    Add files via upload

---
+++ INTERNTASK 3/script.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculateResult() {
+    var expression = document.getElementById('display').value;
+    var result = eval(expression);
+    document.getElementById('display').value = result;
+    
+  }
